9b5c59963107da9ffd055a844a6bcf800abc4628
shareef1992 <shareefahmed1992@gmail.com>
2013-05-02 02:18:31 -0300

    Added comments

--- Reflow_Final/Enter_Time_ino.ino
+++ Reflow_Final/Enter_Time_ino.ino
@@ -1,37 +1,37 @@
 int maxtime;
 int mintime;
 
-int Enter_Time()
+int Enter_Time()                         //Function to enter soak and reflow time
 {
-  lcd_key=read_LCD_buttons();
-  if (moveon == 3){
+  lcd_key=read_LCD_buttons();           //Call button read function
+  if (moveon == 3){                    //Define optimal time ranges for SOAK stage
     maxtime = 120;
     mintime = 60;
-    normalize_number(time);
+    normalize_number(time);            //Call the TempString.INO function to displace correctly
   }
-  if (moveon == 4){
+  if (moveon == 4){                    //Define optimal time ranges for REFLOW stage
     maxtime = 75;
     mintime = 45;
-    normalize_number(time);
+    normalize_number(time);            //Normalize for correct printing like above
   }
 
-  if (time < mintime){
+  if (time < mintime){                 //Prompt user to increase set time if too low
     lcd.setCursor(8,1);
     lcd.print("inc time");
   }
-  if (time > maxtime){
+  if (time > maxtime){                 //Prompt user to decrease set time if too high
     lcd.setCursor(8,1);
     lcd.print("dec time");
   }
 
-  if (mintime <= time && time <= maxtime){
+  if (mintime <= time && time <= maxtime){   //Reset warning message 
     lcd.setCursor(8,1);
     lcd.print("        ");
   }
 
   
-   if (lcd_key == btnSELECT){
-    if (lcd_key == btnSELECT && buttonLast1 != btnSELECT)
+   if (lcd_key == btnSELECT){                                        //Select button moves on to next stage in Reflow_Final.INO
+    if (lcd_key == btnSELECT && buttonLast1 != btnSELECT)            //Debounce function select button
     {
       moveon = moveon + 1;
       lcd.clear();
@@ -43,8 +43,8 @@ int Enter_Time()
   }
 
 
-  if (lcd_key == btnUP){
-    if (lcd_key == btnUP && buttonLast2 != btnUP)
+  if (lcd_key == btnUP){                                 //Button up increase time by 1
+    if (lcd_key == btnUP && buttonLast2 != btnUP)        //Debounce for button up
     {
       time = time + 1;
       normalize_number(time);
@@ -55,15 +55,15 @@ int Enter_Time()
     buttonLast2 = 0;
   }
 
-  if (lcd_key == btnRIGHT){
-      time = time + 10;
+  if (lcd_key == btnRIGHT){                             //Button right increase time by 10                              
+      time = time + 10; 
       normalize_number(time);
       buttonLast3 = btnRIGHT;
       delay(200);
     }
 
-  if (lcd_key == btnDOWN){
-    if (lcd_key == btnDOWN && buttonLast4 != btnDOWN)
+  if (lcd_key == btnDOWN){                             //Button down decreases time by 1    
+    if (lcd_key == btnDOWN && buttonLast4 != btnDOWN)  //debounce for button down
     {
       time = time - 1;
       normalize_number(time);
@@ -74,8 +74,8 @@ int Enter_Time()
     buttonLast4 = 0;
   }
 
-  if (lcd_key == btnLEFT){
-    if (lcd_key == btnLEFT && buttonLast5 != btnLEFT)
+  if (lcd_key == btnLEFT){                             //Button left decreases time by 10  
+    if (lcd_key == btnLEFT && buttonLast5 != btnLEFT)  //Debounce for button left
     {
       time = time - 10;
       normalize_number(time);
@@ -85,6 +85,6 @@ int Enter_Time()
   else {
     buttonLast5 = 0;
   }
-  return time;
-  return moveon;
+  return time;           //Return user-inputed time
+  return moveon;         //Update moveon in the case that the select button was pressed
 }
