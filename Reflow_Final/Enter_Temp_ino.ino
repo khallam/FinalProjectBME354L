36f24cdc5fca587c24dbaec77a33015a93931974
shareef1992 <shareefahmed1992@gmail.com>
2013-05-02 02:05:56 -0300

    Added comments

--- Reflow_Final/Enter_Temp_ino.ino
+++ Reflow_Final/Enter_Temp_ino.ino
@@ -1,4 +1,6 @@
 // Function that allows the user to pick a set temperature
+
+//Define Variables
 int maxtemp;
 int mintemp;
 int buttonLast1=0;
@@ -7,38 +9,38 @@ int buttonLast3=0;
 int buttonLast4=0;
 int buttonLast5=0;
 
-int Enter_Temp()
+int Enter_Temp()                   //Define the function for user input temperature
 {
-  lcd_key=read_LCD_buttons();
+  lcd_key=read_LCD_buttons();      //Call the read buttons function
 
-  if (moveon == 0){
-    maxtemp = 160;
+  if (moveon == 0){                //Moveon is based on Reflow_Final.INO
+    maxtemp = 160;                 //Define safe temperature ranges for SOAK stage
     mintemp = 140;
-    lcd.setCursor(0,1);
+    lcd.setCursor(0,1);            
     lcd.print(temp);
   }
-  if (moveon == 1){
-    maxtemp = 250;
+  if (moveon == 1){               
+    maxtemp = 250;                 //Define safe temperature ranges for REFLOW stage
     mintemp = 218;
     lcd.setCursor(0,1);
     lcd.print(temp);
   }
 
-  if (temp < mintemp){
+  if (temp < mintemp){             //Prompt user to enter a higher temperature
     lcd.setCursor(8,1);
     lcd.print("too low");
   }
-  if (temp > maxtemp){
+  if (temp > maxtemp){             //Prompt user to enter a lower temperature
     lcd.setCursor(8,1);
     lcd.print("too high");
   }
 
-  if (mintemp <= temp && temp <= maxtemp){
+  if (mintemp <= temp && temp <= maxtemp){             //Reset screen to delete prompt for lower/higher temp
     lcd.setCursor(8,1);
     lcd.print("        ");
   }
 
-   if (lcd_key == btnSELECT){
+   if (lcd_key == btnSELECT){                          //Debounce function
     if (lcd_key == btnSELECT && buttonLast1 != btnSELECT)
     {
       moveon = moveon + 1;
@@ -51,8 +53,8 @@ int Enter_Temp()
   }
 
 
-  if (lcd_key == btnUP){
-    if (lcd_key == btnUP && buttonLast2 != btnUP)
+  if (lcd_key == btnUP){                              //Define button up to increase temperature by 1
+    if (lcd_key == btnUP && buttonLast2 != btnUP)     //Debounce for button up
     {
       temp = temp + 1;
       normalize_number(temp);
@@ -63,15 +65,15 @@ int Enter_Temp()
     buttonLast2 = 0;
   }
 
-  if (lcd_key == btnRIGHT){
+  if (lcd_key == btnRIGHT){                            //Define button right to increase temperature by 10          
       temp = temp + 10;
       normalize_number(temp);
       buttonLast3 = btnRIGHT;
       delay(200);
     }
 
-  if (lcd_key == btnDOWN){
-    if (lcd_key == btnDOWN && buttonLast4 != btnDOWN)
+  if (lcd_key == btnDOWN){                             //Define button down to decrease temperature by 1   
+    if (lcd_key == btnDOWN && buttonLast4 != btnDOWN)  //Debounce function for button down
     {
       temp = temp - 1;
       normalize_number(temp);
@@ -82,8 +84,8 @@ int Enter_Temp()
     buttonLast4 = 0;
   }
 
-  if (lcd_key == btnLEFT){
-    if (lcd_key == btnLEFT && buttonLast5 != btnLEFT)
+  if (lcd_key == btnLEFT){                                  //Define button left to decrease temperature by 10   
+    if (lcd_key == btnLEFT && buttonLast5 != btnLEFT)       //Debounce function for button left
     {
       temp = temp - 10;
       normalize_number(temp);
@@ -94,6 +96,6 @@ int Enter_Temp()
     buttonLast5 = 0;
   }
 
-  return temp;
-  return moveon;
+  return temp;                                             //Return the user-set temperature
+  return moveon;                                           //Return moveon
 }
