a9ad0abc27b2e7a6c8bf0c6115226f41d38b357a
shareef1992 <shareefahmed1992@gmail.com>
2013-05-02 01:36:31 -0300

    Added comments

--- Reflow_Final/ButtonPress.ino
+++ Reflow_Final/ButtonPress.ino
@@ -1,18 +1,18 @@
 //Adapted from Dr. Mark Palmeri--4/21/2013
-int buttonPin=A0;
-#define btnRIGHT 0
+int buttonPin=A0;                                 //Read Pin A0
+#define btnRIGHT 0  //Define buttons
 #define btnUP 1
 #define btnDOWN 2
 #define btnLEFT 3
 #define btnSELECT 4
 #define btnNONE 5
 
-int read_LCD_buttons()
+int read_LCD_buttons()            //Function statement to read buttons
 {
   int adcIn;
-  adcIn = analogRead(buttonPin);
+  adcIn = analogRead(buttonPin);               //Read button pin A0
   delay(5); 
-  if (adcIn < 50) return btnRIGHT;
+  if (adcIn < 50) return btnRIGHT;            //Conditions of voltage divider to obtain each button value
   else if (adcIn < 195) return btnUP;
   else if (adcIn < 380) return btnDOWN;
   else if (adcIn < 555) return btnLEFT;
