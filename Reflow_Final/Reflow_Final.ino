bdd3f9c34c3db40d69cf6672f96da8b41e25ddab
shareef1992 <shareefahmed1992@gmail.com>
2013-05-02 02:48:01 -0300

    Added comments

--- Reflow_Final/Reflow_Final.ino
+++ Reflow_Final/Reflow_Final.ino
@@ -9,6 +9,7 @@ LiquidCrystal lcd(8,9,4,5,6,7); //Define pins for LCD display
 int lcd_key = 0;
 int adc_key_in = 0;
 
+//Define buttons and voltage values for LCD
 #define btnRIGHT 0
 #define btnUP 1
 #define btnDOWN 2
@@ -21,6 +22,7 @@ int adc_key_in = 0;
 #define V4 500
 #define V5 800
 
+//Define all variables
 int cTemp = 0;
 int moveon = 0;
 double soaktemp;
@@ -39,7 +41,7 @@ int doneReflow=0;
 
 void setup()
 {
-  lcd.begin(16,2);
+  lcd.begin(16,2); //Begin LCD display
 
 }
 
@@ -52,23 +54,23 @@ void loop()
   //User Input Stage
   if (moveon == 0){
     lcd.setCursor(0,0);
-    lcd.print("Preht Temp, C");
+    lcd.print("Preht Temp, C");              //Input soak temperature based on Enter-Temp.INO
     Enter_Temp();
     soaktemp = temp;
     delay(5);
   }
 
-  if (moveon == 1)
+  if (moveon == 1)                          //Moveon is controlled by Enter_Temp.INO/Enter_Time.INO and controls stage of user input
   {
     lcd.setCursor(0,0);
     lcd.print("Reflow Tem, C");
-    Enter_Temp();
+    Enter_Temp();                           //Input reflow temperature based on Enter-Temp.INO
     reflowtemp = temp;
     delay(5);
   }
 
 
-  if (moveon == 2)
+  if (moveon == 2)         //Clear the screen before moving on to time input
   {
     lcd.clear();
     moveon = moveon + 1;
@@ -78,7 +80,7 @@ void loop()
   if (moveon == 3){
     lcd.setCursor(0,0);
     lcd.print("Soak Time, sec");
-    Enter_Time();
+    Enter_Time();                            //Input soak time based on Enter-Time.INO
     soaktime = time;
     delay(5);
   }
@@ -86,10 +88,10 @@ void loop()
   if (moveon == 4){
     lcd.setCursor(0,0);
     lcd.print("Reflow Time, sec");
-    Enter_Time();
+    Enter_Time();                            //Input reflow time based on Enter-Time.INO
     reflowtime=time;
   }
-  if (moveon == 5) 
+  if (moveon == 5)                           //Clear the screen once all user input is finished
   {
     lcd.setCursor(0,0);
     lcd.print("                ");
@@ -99,10 +101,10 @@ void loop()
     delay(200);
   }
 
-  if (moveon == 6)
+  if (moveon == 6)                    //Display all user input 
   {
     lcd.setCursor(0,0);
-    lcd.print("Sel to Start");
+    lcd.print("Sel to Start");        //Prompt user to select start if values are correct. Press reset otherwise
     lcd.setCursor(0,1);
     lcd.print(soaktemp);
     lcd.setCursor(4,1);
@@ -113,36 +115,36 @@ void loop()
     lcd.print(reflowtime);
     lcd_key=read_LCD_buttons();
     switch(lcd_key){
-    case btnSELECT:
+    case btnSELECT:                     //Select button moves on to start reflow
       moveon = moveon + 1;
       delay(5);
     }
   }
-  if (moveon >= 7)
+  if (moveon >= 7)                      //Start running the reflow curve
   {
-    lcd.setCursor(0,0);
+    lcd.setCursor(0,0);                 //Clear the screen
     lcd.print("                ");
     lcd.setCursor(0,1);
     lcd.print("                ");
     lcd.setCursor(8,1); 
-    curTemp = analogRead(A5);
-    lcd.print(curTemp);
+    curTemp = analogRead(A5);           //Read current temperature
+    lcd.print(curTemp);                 //Print current temperature
     lcd.setCursor(11,1);
-    lcd.print("C");
+    lcd.print("C");                     //Print units of curtemp
     lcd.setCursor(0,1);
-    lcd.print(printtime,DEC);
+    lcd.print(printtime,DEC);           //Print time left in stage
     lcd.setCursor(2,1);
-    lcd.print("s");
+    lcd.print("s");                     //Print units of time
     delay(500);
 
-    stage=Reflow_Stage(curTemp, soaktemp, reflowtemp, soaktime, reflowtime); 
+    stage=Reflow_Stage(curTemp, soaktemp, reflowtemp, soaktime, reflowtime);  //Run through all stages and return time left
 
-    if (doneReflow = 1)
+    if (doneReflow = 1)                  //If reflow is done, record statistics
     { 
       int maxpresoaktemp;
       int presoakerror;
 
-      maxpresoaktemp = EEPROM.read(2);
+      maxpresoaktemp = EEPROM.read(2);              //Read EEPROM max temps and display statistics
       presoakerror = maxpresoaktemp-soaktemp;
       lcd.setCursor(0,0);
       lcd.print(maxpresoaktemp);
