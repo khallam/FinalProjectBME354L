f1f2f94845dd7f345f26ab7abaf973351634de5d
shareef1992 <shareefahmed1992@gmail.com>
2013-05-02 21:58:48 -0300

    Update TempString_ino.ino

--- Reflow_Final/TempString_ino.ino
+++ Reflow_Final/TempString_ino.ino
@@ -1,16 +1,19 @@
 //Function to normalize position of temp/time as to alternate between multiple digit numbers
-int normalize_number(int number)            //Define the function
+
+int normalize_number(int number, int pos1, int pos2)            //Define the function
 {
-  if (number >= 10 && number < 100){       //If two digits, add a space at the 100s digit, then  print number
-    lcd.setCursor(0,1);
+  if (number >= 10 && number < 100){       //If 2 digits, add a space at the 100s digit, then  print number
+    lcd.setCursor(pos1,pos2);
     lcd.print(" ");
     lcd.print(number);
   }
-  
   if (number >= 100 && number < 1000){         //If 3 digits, print number normally. Don't expect more than 3 digits.
-    lcd.setCursor(0,1);
+    lcd.setCursor(pos1,pos2);
     lcd.print(number);
-
   }
-  
+  if (number<10){                         //If 1 digits, print two spaces followed by the digit
+    lcd.setCursor(pos1,pos2);
+    lcd.print("  ");
+    lcd.print(number);
+  }
 }
