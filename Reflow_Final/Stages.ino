155d939877eb863e14e6a7db9b6edda3fd2065e9
shareef1992 <shareefahmed1992@gmail.com>
2013-05-02 21:57:26 -0300

    Update Stages.ino

--- Reflow_Final/Stages.ino
+++ Reflow_Final/Stages.ino
@@ -1,28 +1,33 @@
+//Control of stages of reflow curve for Reflow_Final.INO
+
+//Define variables
 int refstage = 0; //Constant to control progress of stages
 int curtime; //from STOPWATCH.INO
-String dispstage;
+String dispstage;          
 unsigned long lastM;
-int maxPeak=0;
+int maxPeak1=0;
+int maxPeak2=0;
+int presoakerr;
+int reflowerr;
 
 char Reflow_Stage(int curtemp, int soaktemp, int reflowtemp, int soaktime, int reflowtime) //Function to run through all stages--Used in REFLOW_FINAL.INO
 {
   if (refstage == 0){ //This stage simply records time to reach this point, and freezes lastM
     lastM = millis();
-    refstage = 1; //run and move on to Pre-Soak
+    refstage = 1; //move on to Pre-Soak
   }
 
   if (refstage == 1){                              //Pre-Soak stage--dependent upon temperature condition
     dispstage = "Presoak";
     Control(soaktemp,curtemp,lastM);               //PID function modulates temp to soaktemp 
-
-    if (curtemp >= soaktemp){                      //Condition to move on to soak stage
-      refstage = 2;
-      lastM=millis();                              //Freeze millis time as lastM
-    }
-
     lcd.setCursor(3,0);                            //Print current stage
     lcd.print(dispstage);
-    delay(500);                                    //Take measurements every 0.5 seconds
+
+    if (curtemp >= soaktemp){                      //Condition to move on to soak stage
+      lcd.clear();                                 //Clear the screen before moving on
+      refstage = 2;
+      lastM=millis();                              //Freeze millis time as lastM before moving on
+    }
   }
 
   if (refstage == 2){                              //Soak stage--dependent upon time condition
@@ -32,18 +37,17 @@ char Reflow_Stage(int curtemp, int soaktemp, int reflowtemp, int soaktime, int r
     lcd.setCursor(3,0);
     lcd.print(dispstage);                          //Print current stage
     Control(soaktemp,curtemp,lastM);               //PID functions keeps temp at soaktemp
-    delay(500);                                    //Delay 0.5 seconds between measurements
 
-    if (maxPeak <= curTemp){                       //Record max temp of this SOAK
-      maxPeak = curTemp;
+    if (maxPeak1 <= curTemp){                       //Record max temp of this SOAK
+      maxPeak1 = curTemp;
     }
-    else if (maxPeak>curTemp){                     //Save max temp of SOAK to EEPROM 1
-      maxPeak = maxPeak;
-      EEPROM.write(1,maxPeak);
+    else if (maxPeak1>curTemp){         
+      maxPeak1 = maxPeak1;
     }
 
     if (curtime >= soaktime){                      //Time condition to move on to pre-reflow
-      refstage = 3;
+      refstage = 3; 
+      lcd.clear();                                 //Clear the screen before moving on
     }
   }
 
@@ -51,76 +55,94 @@ char Reflow_Stage(int curtemp, int soaktemp, int reflowtemp, int soaktime, int r
     dispstage = "PreRef";
     lcd.setCursor(3,0);
     lcd.print(dispstage);                          //Print current stage
-    delay(500);                                    //Delay 0.5 seconds between measurements
 
-    Control(reflowtemp,curtemp,lastM);             //This PID ramps up until the temperature reaches reflow temp
+      Control(reflowtemp,curtemp,lastM);             //This PID ramps up until the temperature reaches reflow temp
 
     if (curtemp >= 217){                           //Condition to move on to reflow temp --NOT user input (Reflow starts at 217 C)
+      lcd.clear();                                 //Clear the screen before moving on
       refstage = 4;
-      lastM=millis();                              //Freeze millis as lastM
+      lastM=millis();                              //Freeze millis as lastM before moving on
     }
   }
 
   if (refstage == 4){                              //Reflow Stage--Time dependent
     dispstage = "Reflow";
     curtime = Stopwatch(reflowtime,lastM);         //Current time based on zeroed time
-    printtime=reflowtime-curtime;                 //Display time left
+    printtime=reflowtime-curtime;                  //Display time left
 
     lcd.setCursor(3,0);
     lcd.print(dispstage);                          //Display current stage
-    delay(500);                                    //Delay 0.5 seconds between measurements
 
-    if (maxPeak <= curTemp){                       //Find max temp of REFLOW
-      maxPeak = curTemp;
+      if (maxPeak2 <= curTemp){                       //Find max temp of REFLOW
+      maxPeak2 = curTemp;
     }
-    else if (maxPeak>curTemp){                     //Save max temp of REFLOW to EEPROM 2
-      maxPeak = maxPeak;
-      EEPROM.write(2,maxPeak);
+    else if (maxPeak2>curTemp){               
+      maxPeak2 = maxPeak2;
     }
 
     Control(reflowtemp,curtemp,lastM);             //PID function to keep temp at reflow temp
 
     if (curtime >= reflowtime){                    //Condition to move on to cooling stage
+      lcd.clear();                                 //Clear the screen before moving on
       refstage = 5;
     }
   }
 
   if (refstage == 5){                              //Cooling stage--Dependent on temperature
     dispstage = "Cooling (FAN ON)";
-    lcd.setCursor(3,0);
+    lcd.setCursor(0,0);
     lcd.print(dispstage);                          //Display stage
-    delay(500);                                    //Delay 0.5 seconds before each sample
 
-    Control(25,curtemp,lastM);                     //PID controls temperature at room temperature
+      Control(25,curtemp,lastM);                     //PID controls temperature at room temperature
+
+    lcd.setCursor(1,1);                            //Print the max soak and reflow temp while cooling the oven
+    lcd.print(maxPeak1);
+    lcd.setCursor(5,1);
+    lcd.print(maxPeak2);
+    delay(200);                                    //Delay so temperature updates at a rate 0.2 sec
 
     if (curtemp < 40){                             //Move on to next stage once temperature is safe to touch
+      lcd.clear();                                 //Clear the screen before moving on
       refstage = 6;
     }
   }
 
-  if (refstage == 6){                              //Clear the screen
-    lcd.setCursor(0,0);
-    lcd.print("                ");
-    lcd.setCursor(0,1);
-    lcd.print("                ");
-    refstage == 7;
-  }
+  if (refstage == 6){                              //Reflow Run over
+    lcd.setCursor(0,0);                            //Print that it is safe to touch oven and that reflow is done.
+    lcd.print("Done, safe");
+    digitalWrite(RelayPin,LOW);                    //Make sure oven is turned off after this point
+    lcd.setCursor(1,1);
 
-  if (refstage == 7){                              //Reflow Run over
-    lcd.setCursor(0,0);
-    lcd.print("Done");
-    lcd.setCursor(0,1);
-    lcd.print("Safe to touch"); 
-    doneReflow=1;  
+    //Convert the saved max peak values into doubles in order to perform the percent error calculation
+    //Without converting to doubles, the percent error is 0
+    double maxPeak1Print = maxPeak1;
+    double maxPeak2Print = maxPeak2;
+    double soaktempPrint = soaktemp;
+    double reflowtempPrint = reflowtemp;
+
+    //Calculate percent error and display percent error
+    presoakerr = (abs((maxPeak1Print-soaktempPrint))/soaktempPrint)*100;
+    lcd.print(presoakerr);
+    lcd.setCursor(3,1);
+    lcd.print("%");
+
+    reflowerr = (abs((maxPeak2Print-reflowtempPrint))/reflowtempPrint)*100;
+    lcd.setCursor(5,1);
+    lcd.print(reflowerr);
+    lcd.setCursor(7,1);
+    lcd.print("%");
+
+    delay(500);
   }
 
   if (curtemp > 250){                             //Software safety condition
     lcd.clear();
+    digitalWrite(RelayPin,LOW);                     //Turn PID off
     lcd.setCursor(0,0);
     lcd.print("TOO HOT");
     lcd.setCursor(0,1);
-    lcd.print("TURN OFF!!");
+    lcd.print("TURN OFF!!");                        //Prompt user to manually turn off oven
+    delay(200);
   }
   return printtime;
 }
-
