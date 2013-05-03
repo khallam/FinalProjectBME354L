415eb4ff4afab05b7f8cce01a24b2f90e934ceed
shareef1992 <shareefahmed1992@gmail.com>
2013-05-02 21:56:43 -0300

    Delete all code

--- Reflow_Final/Reflow_input_ino.ino
+++ Reflow_Final/Reflow_input_ino.ino
@@ -1,17 +1,1 @@
-
-
-/*
- 
- //Function that displays the set temperature chosen in the above function
- int SetTempDisp()
- {  
- lcd.setCursor(0,1);
- lcd.print("Set Temp:");
- lcd.setCursor(10,1);
- lcd.print(temp);
- delay(500);
- }
- */
-
-//Code that illustrates the functionality of our functions. Look, it is very modular! All we had to do was call the functions we already made. We are beating the "noob" status!
-
+//Useless function--Can't delete
